4d256f72af4d60b950c0b37bcee7ede416cd90e5
yasoo <you@example.com>
2026-05-05 21:59:10 +0900

    fix camera

--- src/components/WebGLCamera.tsx
+++ src/components/WebGLCamera.tsx
@@ -168,8 +168,8 @@ export function WebGLCamera({
       stream = await navigator.mediaDevices.getUserMedia({
         video: {
             facingMode: { ideal: "environment" },
-            width: { ideal: 1920, max: 3840 },
-            height: { ideal: 1080, max: 2160 },
+            width: { ideal: 3840},
+            height: { ideal: 2160},
         },
         audio: false,
       })
